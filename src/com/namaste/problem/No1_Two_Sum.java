13ffc2ee6a4b4d3cfa8b5ab594409a68134d7449
loozhang <loozhang@163.com>
2018-09-04 15:05:38 +0800

    update

--- src/com/namaste/problem/No1_Two_Sum.java
+++ src/com/namaste/problem/No1_Two_Sum.java
@@ -1,5 +1,29 @@
 package com.namaste.problem;
 
 public class No1_Two_Sum {
-
+	public static int[] twoSum(int[] nums, int target) {
+	    for (int i = 0; i < nums.length; i++) {
+	        for (int j = i + 1; j < nums.length; j++) {
+	            if (nums[j] == target - nums[i]) {
+	                return new int[] { i, j };
+	            }
+	        }
+	    }
+	    throw new IllegalArgumentException("No two sum solution");
+	}
+	
+	public static void main(String[] args) {
+		int[] nums =new int[]{2, 7, 11, 15};
+		int target = 9;
+		Print(twoSum(nums,target));
+	}
+	
+    public static void Print(int[] arr)
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]+",");
+        }
+        System.out.println("-------------------------");
+    }
 }
