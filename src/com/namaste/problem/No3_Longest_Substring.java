8b47455cf8bbcef3a03420ffa74c47724fef1c9f
loozhang <loozhang@163.com>
2018-09-06 00:34:31 +0800

    update 3

--- src/com/namaste/problem/No3_Longest_Substring.java
+++ src/com/namaste/problem/No3_Longest_Substring.java
@@ -40,8 +40,25 @@ public class No3_Longest_Substring {
 		return ans;
 	}
 	
+	public static int SlidingWindowOptimized(String s) {
+		int n=s.length(),ans=0;
+		Map<Character,Integer> map=new HashMap<>();
+		for(int j=0,i=0;j<n;j++) {
+			if(map.containsKey(s.charAt(j))) {
+				i=Math.max(map.get(s.charAt(j)), i);
+				//如果发现了重复字符，则比较重复字符所在位置（的下一位）与当前i值的大小，取更大的一个，也就是说，如果重复字符在i之前，则不会影响i的取值，也就不会影响
+				//字串长度的计算，如果重复字符在i后，那就将i拖后到重复字符的下一位
+			}
+			ans=Math.max(ans, j-i+1);
+			map.put(s.charAt(j), j+1);
+		}
+		return ans;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(BruteForce("lyuydifnelqwzrfdvjyyizcczjwosklfigenojauwtqwkybijbgrajwbusqmcbuk"));
-		System.out.println(SlidingWindow("lyuydifnelqwzrfdvjyyizcczjwosklfigenojauwtqwkybijbgrajwbusqmcbuk"));
+		String testStr="lyuydifnelqwzrfdvjyyizcczjwosklfigenojauwtqwkybijbgrajwbusqmcbuk";
+		System.out.println(BruteForce(testStr));
+		System.out.println(SlidingWindow(testStr));
+		System.out.println(SlidingWindowOptimized(testStr));
 	}
 }
