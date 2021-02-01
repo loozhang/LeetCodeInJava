af81a849878d5dbdac71fa522dd3b254f112c401
张陆 <zhanglu70@meituan.com>
2021-02-01 14:43:40 +0800

    update

--- src/com/namaste/problem/No28_Implement_Str_Str.java
+++ src/com/namaste/problem/No28_Implement_Str_Str.java
@@ -1,22 +1,23 @@
 package com.namaste.problem;
 
 public class No28_Implement_Str_Str {
+    /*
+    my solution
+     */
     public static int strStr(String haystack, String needle) {
-        char[] str1=haystack.toCharArray();
-        char[] str2=needle.toCharArray();
         int j,k;
-        if(str2.length==0){
+        if(needle.length()==0){
             return 0;
         }
-        if(str1.length>=str2.length){
-            for(int i=0;i<str1.length;i++){
+        if(haystack.length()>=needle.length()){
+            for(int i=0;i<haystack.length();i++){
                 j=0;k=i;
-                while(str1[k++]==str2[j++]){
-                    if(k==str1.length){
-                        return -1;
-                    }
-                    if(j==str2.length){
+                while(haystack.charAt(k++)==needle.charAt(j++)){
+                    if(j==needle.length()){
                         return i;
+                    }
+                    if(k==haystack.length()){
+                        return -1;
                     }
                 }
             }
@@ -24,10 +25,27 @@ public class No28_Implement_Str_Str {
         return -1;
     }
 
+    /*
+    Standard Solution
+     */
+    public static int strStr1(String haystack, String needle){
+        if(needle.length()==0) return 0;
+        int m=haystack.length(),n=needle.length();
+        if(m<n) return -1;
+        for(int i=0;i<=m-n;i++){
+            int j=0;
+            for(j=0;j<n;j++){
+                if(haystack.charAt(i+j)!=needle.charAt(j)) break;
+            }
+            if(j==n) return i;
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        String haystack="mississippi";
-        String needle="issipi";
-        int a=strStr(haystack,needle);
+        String haystack="a";
+        String needle="a";
+        int a=strStr1(haystack,needle);
         System.out.println(a);
     }
 }
