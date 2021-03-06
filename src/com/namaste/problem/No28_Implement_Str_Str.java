a90643ad278146123707abea4437f07e9ac8091e
loozhang <loozhang@163.com>
2021-03-06 23:13:17 +0800

    update

--- src/com/namaste/problem/No28_Implement_Str_Str.java
+++ src/com/namaste/problem/No28_Implement_Str_Str.java
@@ -1,7 +1,11 @@
 package com.namaste.problem;
 
 public class No28_Implement_Str_Str {
-    public static int strStr(String haystack, String needle) {
+
+    /*
+    my solution
+     */
+    public static int strStr1(String haystack, String needle) {
         char[] str1=haystack.toCharArray();
         char[] str2=needle.toCharArray();
         int j,k;
@@ -17,6 +21,30 @@ public class No28_Implement_Str_Str {
                     }
                     if(j==str2.length){
                         return i;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    /*
+    standard solution
+     */
+    public static int strStr(String haystack, String needle) {
+        int j,k;
+        if(needle.length()==0){
+            return 0;
+        }
+        if(haystack.length()>=needle.length()){
+            for(int i=0;i<haystack.length();i++){
+                j=0;k=i;
+                while(haystack.charAt(k++)==needle.charAt(j++)){
+                    if(j==needle.length()){
+                        return i;
+                    }
+                    if(k==haystack.length()){
+                        return -1;
                     }
                 }
             }
